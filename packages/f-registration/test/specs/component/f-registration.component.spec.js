214fc9f4a788658a329e3f0fd8abc0485d95ceca
David Banks <david.banks@just-eat.com>
2020-09-18 10:21:40 +0100

    WCB-748:  Add test for valid form.

--- packages/f-registration/test/specs/component/f-registration.component.spec.js
+++ packages/f-registration/test/specs/component/f-registration.component.spec.js
@@ -33,6 +33,36 @@ describe('f-registration component tests', () => {
         expect(RegistrationComponent.errorSummaryContainerMessageText()).toBe('There are 4 errors in the form.');
     });
 
+    it.only('should not display errors if all fields are valid', () => {
+        // Arrange
+        const userInfo = {
+            firstName: 'DummyFirstName',
+            lastName: 'DummyLastName',
+            email: 'dummy@example.com',
+            password: 'dummy-password'
+        };
+
+        expect(RegistrationComponent.errorSummaryContainerRole()).toBe('alert');
+        expect(RegistrationComponent.errorSummaryContainerMessageCount()).toBe(0);
+        expect(RegistrationComponent.errorSummaryContainerDisplayed()).toBe(false);
+
+        // Act
+        RegistrationComponent.submitRegistrationForm(userInfo);
+        // browser.debug();
+        // Assert
+        expect(RegistrationComponent.isFirstNameEmptyErrorDisplayed()).toBe(false);
+        expect(RegistrationComponent.isLastNameEmptyErrorDisplayed()).toBe(false);
+        expect(RegistrationComponent.isEmailEmptyErrorDisplayed()).toBe(false);
+        expect(RegistrationComponent.isPasswordEmptyErrorDisplayed()).toBe(false);
+
+        // INFO:  Currently the call to the API on submit is not implemented,
+        // as such there is an error displayed when the form is valid.
+        expect(RegistrationComponent.errorSummaryContainerRole()).toBe('alert');
+        expect(RegistrationComponent.errorSummaryContainerMessageCount()).toBe(1);
+        expect(RegistrationComponent.errorSummaryContainerDisplayed()).toBe(true);
+        expect(RegistrationComponent.errorSummaryContainerMessageText()).toBe('TypeError: Cannot read property \'protocol\' of undefined');
+    });
+
     it('should show and be able to click the legal documentation', () => {
         // Act
         RegistrationComponent.waitForRegistrationForm();
